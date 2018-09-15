d3d65313ff9ae7e9bad1b4c8828a20e4ee033aec
Aleksey <alekseymishuk@gmail.com>
2018-09-15 19:46:45 +0300

    love triangles has been found

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  return preferences.reduce((count, value, index, loveArray) => { 
+    return loveArray[loveArray[value - 1] - 1] === (index + 1) && value !== loveArray[value - 1] ? ++count : count;
+  }, 0) / 3;
 };
